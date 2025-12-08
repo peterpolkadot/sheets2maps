b7238205b27e0be410b1a8a73183ca9a0727f725
peterpolkadot <154844362+peterpolkadot@users.noreply.github.com>
2025-12-08 16:36:46 +1000

    Sheets2Maps auto-commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,4 @@
 
 export default function Home() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Sheets2Maps Working!</h1>
-      <p>This is live from Azure → GitHub → Vercel.</p>
-    </div>
-  );
+  return <h1>Sheets2Maps Working!</h1>;
 }
